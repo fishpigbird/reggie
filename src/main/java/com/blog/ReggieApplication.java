18dd642839fb1b4ee6831e1fb8886e4c788e4e89
fish <fishpigbird@outlook.com>
2023-03-19 22:01:47 +0800

    变更测试

--- src/main/java/com/blog/ReggieApplication.java
+++ src/main/java/com/blog/ReggieApplication.java
@@ -1,6 +1,6 @@
 package com.blog;
 
-import com.blog.utils.MailUtils;
+//import com.blog.utils.MailUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,5 +15,6 @@ public class ReggieApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功...");
+        //git test
     }
 }
